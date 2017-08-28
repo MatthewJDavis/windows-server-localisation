6c5d0eaa4e0e13cce8ba8eccaa5d55aaaae1d778
= <matthewdavis111@gmail.com>
2017-08-28 20:59:42 +0100

    updated unit tests

--- windows_server_localisation/spec/unit/recipes/uk_spec.rb
+++ windows_server_localisation/spec/unit/recipes/uk_spec.rb
@@ -6,15 +6,15 @@
 
 require 'spec_helper'
 
-describe 'windows_uk_config::default' do
-  context 'When all attributes are default, on an Ubuntu 16.04' do
+describe 'windows_server_localisation::uk' do
+  context 'When all attributes are default, on Windows 2016' do
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
       # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
+      runner = ChefSpec::ServerRunner.new(platform: 'windows', version: '2012r2')
       runner.converge(described_recipe)
     end
-
+    #todo stub get-module command
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
